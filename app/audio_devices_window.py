0a5646e3994df65f033f1d64a23126c5f6381001
Asen Dimitrov <sholev@users.noreply.github.com>
2024-06-06 17:29:45 +0300

    add sorting for device frames

--- app/audio_devices_window.py
+++ app/audio_devices_window.py
@@ -4,7 +4,6 @@ import customtkinter_extensions as ctk_e
 from audio_devices import AudioDevices, AudioDevice
 from config import config
 
-
 TXT_MUTE = {
     True: "🔈",
     False: "🔊"
@@ -21,12 +20,35 @@ class AudioDevicesWindow(ctk_e.CTkToplevel):
         self.grid_rowconfigure(1, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
-        self.audio_devices = AudioDevices.get_all_devices()
+        self.audio_devices = self.get_audio_devices_grouped()
+        self.audio_devices = self.sort_audio_devices(self.audio_devices)
         self.label = None
         self.scroll_frame = None
         self.visibility_label = None
         self.states_combo_box = None
         self.build_widgets()
+
+    @staticmethod
+    def sort_audio_devices(audio_devices: dict[str, list[AudioDevice]]):
+        if config.get('audio_devices_order') is None:
+            config.set('audio_devices_order', list(audio_devices.keys()))
+
+        order = config.get('audio_devices_order')
+        for k in audio_devices:
+            if k not in order:
+                order.append(k)
+
+        return {k: audio_devices[k] for k in order if k in audio_devices}
+
+    @staticmethod
+    def get_audio_devices_grouped() -> dict[str, list[AudioDevice]]:
+        audio_devices = AudioDevices.get_all_devices()
+        devices_per_type = {}
+        for device in audio_devices:
+            device_name = device.get_device_name()
+            devices_per_type.setdefault(device_name, []).append(device)
+
+        return devices_per_type
 
     def build_widgets(self):
         self.label = ctk.CTkLabel(
@@ -78,42 +100,81 @@ class AudioDevicesWindow(ctk_e.CTkToplevel):
 
 
 class AudioDevicesFrame(ctk.CTkScrollableFrame):
-    def __init__(self, master, audio_devices: list[AudioDevice], **kwargs):
+    def __init__(self, master, audio_devices: dict[AudioDevice], **kwargs):
         super().__init__(master, **kwargs)
 
         self.configure(fg_color=('gray72', 'gray22'))
         self.grid_columnconfigure(0, weight=1)
 
         self.audio_devices = audio_devices
-        devices_per_type = {}
-        for device in self.audio_devices:
-            device_name = device.get_device_name()
-            devices_per_type.setdefault(device_name, []).append(device)
+        self.audio_device_frames = self.create_frames()
 
-        self.audio_device_frames = []
-        for name, devices in devices_per_type.items():
-            count = len(self.audio_device_frames)
-            frame = DeviceFrame(self, title=name, devices=devices)
+        self.draw_frames_grid()
+
+    def create_frames(self):
+        frames = []
+        for name, devices in self.audio_devices.items():
+            frame = DeviceFrame(
+                self, title=name, devices=devices, on_move=self.on_move_frame
+            )
+            frames.append(frame)
+
+        return frames
+
+    def draw_frames_grid(self, start_row=1):
+        for i, frame in enumerate(self.audio_device_frames):
             frame.grid(
-                row=1 + count, column=0, padx=10, pady=10, sticky="nsew")
-            self.audio_device_frames.append(frame)
+                row=start_row + i, column=0, padx=10, pady=10, sticky="nsew"
+            )
 
     def on_state_selected(self, event: str):
         for frame in self.audio_device_frames:
             frame.on_state_selected(event)
 
+    def sort_and_draw_frames_grid(self):
+        devices_order = config.get('audio_devices_order')
+        self.audio_device_frames = sorted(
+            self.audio_device_frames,
+            key=lambda f: devices_order.index(f.title)
+        )
+
+        self.draw_frames_grid()
+
+    def on_move_frame(self, option: str, title: str):
+        if option == 'Top':
+            devices_order = config.get('audio_devices_order')
+            devices_order.remove(title)
+            devices_order.insert(0, title)
+        elif option == 'Bottom':
+            devices_order = config.get('audio_devices_order')
+            devices_order.remove(title)
+            devices_order.append(title)
+
+        self.sort_and_draw_frames_grid()
+        self.on_state_selected(config.get('selected_device_state'))
+
 
 class DeviceFrame(ctk_e.CTkVisibilityGridFrame):
-    def __init__(self, master, title: str, devices: list[AudioDevice],
+    def __init__(self, master, title: str, devices: list[AudioDevice], on_move,
                  **kwargs):
         super().__init__(master, **kwargs)
 
         self.devices = devices
 
         self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
 
+        self.title = title
         self.title_label = ctk.CTkLabel(self, text=title)
         self.title_label.grid(row=0, column=0, padx=10, pady=1, sticky='w')
+
+        self.move_option_menu = ctk.CTkOptionMenu(
+            self, values=['Top', 'Bottom'],
+            command=lambda option: on_move(option, self.title)
+        )
+        self.move_option_menu.grid(row=0, column=1, padx=10, pady=1,
+                                   sticky='e')
+        self.move_option_menu.set('Move')
 
         sliders_per_flow = {}
         for device in devices:
@@ -124,7 +185,8 @@ class DeviceFrame(ctk_e.CTkVisibilityGridFrame):
         for flow, flow_devices in sliders_per_flow.items():
             count = len(self.flow_frames)
             frame = FlowFrame(self, title=flow, devices=flow_devices)
-            frame.grid(row=1 + count, column=0, padx=0, pady=1, sticky="ew")
+            frame.grid(row=1 + count, column=0, columnspan=2, padx=0, pady=1,
+                       sticky="ew")
             self.flow_frames.append(frame)
 
     def on_state_selected(self, event: str):
